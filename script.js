e39a65956b7f13e392db8e95fb9095ebecd56370
AFoo97 <arthur.foozb@gmail.com>
2025-01-28 20:14:57 +1100

    Added function to get human choice

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function getComputerChoice() {
   return choices[randomChoice];
 }
 
+//Function to get human choice
+function getHumanChoice() {
+    let humanChoice = prompt("Enter your choice: rock, paper or scissors").toLowerCase();
+
+    //Input validation
+    while (!["rock", "paper", "scissors"].includes(humanChoice)) {
+        humanChoice = prompt("Invalid choice. Please enter rock, paper or scissors").toLowerCase();
+    }
+
+    return humanChoice;
+}
